71f1b21a9266bf4c00f3ec13a137f56b2affec70
JT Turner <j.tturner14@yahoo.com>
2020-03-21 13:22:36 -0400

    landing and sign up css

--- client/src/components/loginForm/loginForm.js
+++ client/src/components/loginForm/loginForm.js
@@ -3,27 +3,28 @@ import "./style.css";
 
 export default function loginForm() {
 
-  return (
-    <div className = 'container'>
-        <div className="field">
-            <p class="control has-icons-left has-icons-right">
-                <input className="input" type="email" placeholder="Email"/>
-                    <span className="icon is-small is-left">
-                        <i className="fas fa-envelope"></i>
-                    </span>
-                    <span className="icon is-small is-right">
-                        <i className="fas fa-check"></i>
-                    </span>
-            </p>
-         </div>
-            <div className="field">
-                <p className="control has-icons-left">
-                    <input className="input" type="password" placeholder="Password"/>
+    return (
+        <div className='container tile is-4 is-parent box'>
+            <div className="tile is-child">
+                <div className="field">
+                    <p class="control has-icons-left has-icons-right">
+                        <input className="input" type="email" placeholder="Email" />
+                        <span className="icon is-small is-left">
+                            <i className="fas fa-envelope"></i>
+                        </span>
+                        <span className="icon is-small is-right">
+                            <i className="fas fa-check"></i>
+                        </span>
+                    </p>
+                </div>
+                <div className="field">
+                    <p className="control has-icons-left">
+                        <input className="input" type="password" placeholder="Password" />
                         <span className="icon is-small is-left">
                             <i className="fas fa-lock"></i>
                         </span>
-                </p>
-            </div>
+                    </p>
+                </div>
                 <div className="field">
                     <p className="control">
                         <button className="button is-success loginbtn">
@@ -32,7 +33,8 @@ export default function loginForm() {
                     </p>
                     <h5><a href="#">Forgot Password?</a></h5>
                 </div>
-    </div>
+            </div>
+        </div>
 
     )
 }
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import Navbar from './../components/navbar';
+import './design/Landing.css'
 
 function Landing() {
     return (
         <div>
             <Navbar />
-            <div className="tile is-ancestor">
-                <div className="tile is-7 is-vertical is-parent">
-                    <div className="tile is-child box">
-                        <figure className="image is-128x128">
-                            <img id="userPic" src="https://bulma.io/images/placeholders/128x128.png" />
-                            <button id="addImage">Add profile picture</button>
-                        </figure>
-                        <br/>
-                        <p className="title" id="username">JT Turner</p>
-                        <p id="funds">Funds Available: $100</p>
-                        <p id="member">Member Since: April 2019</p>
+            <div className="outerTile">
+                <div className="tile is-10 container is-fluid">
+                    <div className="tile is-7 is-vertical is-parent">
+                        <div className="tile is-child box">
+                            <figure className="image is-128x128">
+                                <img id="userPic" src="https://bulma.io/images/placeholders/128x128.png" />
+                                <button id="addImage">Add profile picture</button>
+                            </figure>
+                            <br />
+                            <p className="title" id="username">JT Turner</p>
+                            <p id="funds">Funds Available: $100</p>
+                            <p id="member">Member Since: April 2019</p>
+                        </div>
+                        <div className="tile is-child box">
+                            <p className="title" id="paymentList">Payments</p>
+                            <p>Lisa paid Arnold for pizza</p>
+                            <p>Jeff paid Jesus for rent</p>
+                        </div>
+
                     </div>
-                    <div className="tile is-child box">
-                        <p className="title" id="paymentList">Payments</p>
-                        <p>Lisa paid Arnold for pizza</p>
-                        <p>Jeff paid Jesus for rent</p>
-                    </div>
-                    
-                </div>
-                <div className="tile is-3 is-child box">
+                    <div className="tile is-3 is-child box">
                         <p className="title" id="friendList">Friends</p>
                         <article className="media">
                             <figure className="media-left">
@@ -117,9 +119,11 @@ function Landing() {
                             </div>
                         </article>
                     </div>
-            </div>
+                </div>
 
+            </div>
         </div>
+
 
 
     )
