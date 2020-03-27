e6a3fc8ba119d48081b096c614fee0e4aa267d24
Mamessile Kiman <sile.kiman@yahoo.com>
2020-03-27 18:55:03 -0400

    merging code from master

--- client/src/pages/SignUp.js
+++ client/src/pages/SignUp.js
@@ -4,9 +4,7 @@ import API from "../utils/api";
 import Navbar from './../components/navbar';
 import Alert from "../components/Alert/alert"
 import './signup.css'
-import {BrowserRouter as Router,Route, Redirect,Switch} from 'react-router-dom';
 import Login from "./Login";
-import Home from './About'
 
 
 function SignUp(){
