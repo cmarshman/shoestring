d96a0e7653e8790606b2cb610beab135339dc38b
DeVante Bailey <devantebailey1322@gmail.com>
2020-04-04 03:03:46 -0400

    readme

--- client/src/components/FriendCard/index.js
+++ client/src/components/FriendCard/index.js
@@ -1,19 +1,25 @@
 import React from "react";
 import friends from "../../utils/friendList.json";
+import './style.css';
+import { Link, withRouter } from 'react-router-dom';
 
 
 function Card() {
+
+    
     return (
-        <div className="tile is-child box has-text-centered">
+        <div className="tile is-child box has-text-centered" id="pinkDuck">  
             {friends.map(item => (
-                <article key={item.id} className="media">
+                <article key={item.id} className="media is-scrollable">
                     <figure className="media-left" id="block">
-                        <p className="image is-48x48" id="friendPic">
-                            <img className="is-rounded"src="https://bulma.io/images/placeholders/96x96.png" alt="userImage" />
+                        <p className="image is-square is-48x48" id="friendPic">
+                            <img className="is-rounded" src={item.image} alt="userImage" />
                         </p>
                     </figure>
                     <div>
-                        <h1 className="has-text-weight-semibold has-text-left" id="name">{item.name}</h1>
+
+                        <Link to={`/user-profile/${item.name}`} className="title" id={item.name}>{item.name}</Link>
+
                         <h3 className="has-text-left" id="location">{item.location}</h3>
                     </div>
 
@@ -24,4 +30,4 @@ function Card() {
     );
 }
 
-export default Card;
+export default withRouter (Card);
--- controllers/controller.js
+++ controllers/controller.js
@@ -5,7 +5,6 @@ var PLAID_CLIENT_ID = "5e45d9e00f9243001237c447";
 var PLAID_SECRET = "3e32fe89ce9a6c586bd001cf7be2f3";
 var PLAID_PUBLIC_KEY = "a470a31fd930e601383597d010adba";
 var PLAID_ENV = "development";
-
 var ACCESS_TOKEN = null;
 var PUBLIC_TOKEN = null;
 var ITEM_ID = null;
@@ -23,7 +22,9 @@ const receivePublicToken = (req, res) => {
   // First, receive the public token and set it to a variable
   let PUBLIC_TOKEN = req.body.public_token;
   // Second, exchange the public token for an access token
-  client.exchangePublicToken(PUBLIC_TOKEN, function(error, tokenResponse) {
+
+  client.exchangePublicToken(PUBLIC_TOKEN, function (error, tokenResponse) {
+
     ACCESS_TOKEN = tokenResponse.access_token;
     ITEM_ID = tokenResponse.item_id;
     res.json({
@@ -50,9 +51,11 @@ const getTransactions = (req, res) => {
       count: 250,
       offset: 0
     },
-    function(error, transactionsResponse) {
+
+    function (error, transactionsResponse) {
       res.json({ transactions: transactionsResponse });
-      // TRANSACTIONS LOGGED BELOW! 
+      // TRANSACTIONS LOGGED BELOW!
+
       // They will show up in the terminal that you are running nodemon in.
       console.log(transactionsResponse);
     }
@@ -63,3 +66,4 @@ module.exports = {
   receivePublicToken,
   getTransactions
 };
+
