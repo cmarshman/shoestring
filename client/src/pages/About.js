917d4ad76c37acea7db10c6ea977eb81bc5755e8
Cory Marshman <marshman.cory@gmail.com>
2020-04-04 03:26:26 -0400

    setting up herok

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -18,10 +18,9 @@ function About() {
                         </div>
                     </div>
                     <div className="column is-three-fifths">
-                        <p className="title">About shoestring!</p>
+                        <p className="title">Meet the Team</p>
                         <br />
-                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam tristique tempus volutpat. Vivamus nibh mi, vestibulum sed eleifend sed, finibus non lectus. Mauris lobortis, dui et cursus scelerisque, tellus lectus efficitur augue, in pellentesque ipsum nunc eget odio. Nullam vel lacus ut est malesuada tincidunt. Quisque sapien arcu, dictum non tincidunt vitae, fringilla non lectus. Vivamus nibh felis, consectetur molestie elit et, hendrerit tincidunt dolor.
-                        </p>
+                        <TheTeam />
                         <br />
                         <p className="subtitle"><strong>Download our app</strong></p>
                         <div className="columns">
@@ -31,13 +30,6 @@ function About() {
                 </div>
             </div>
             <br />
-            <div className="tile is-ancestor">
-                <div className="tile is vertical is-10 is-clearfix columns" id="tile">
-                    <p className="title">Meet the Team</p>
-                </div>
-            </div>
-            <br />
-            <TheTeam />
         </>
 
     );
