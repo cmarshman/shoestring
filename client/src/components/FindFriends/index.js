61872e74f3f5408c80a3b78323f8417535d522f7
JT Turner <j.tturner14@yahoo.com>
2020-04-13 19:12:23 -0400

    removed button from friend search

--- client/src/components/FindFriends/index.js
+++ client/src/components/FindFriends/index.js
@@ -7,9 +7,9 @@ function FindFriends () {
     
     const [pickSearch, setPickSearch] = useState(false)
     
-    const handleToggle = () => {
-        setPickSearch(pickSearch => !pickSearch)
-    }
+    // const handleToggle = () => {
+    //     setPickSearch(pickSearch => !pickSearch)
+    // }
 
     return(
         <>
@@ -18,13 +18,17 @@ function FindFriends () {
                     <div className="tile is-10 container column is-fluid">
                         <div className="tile is-12 is-vertical is-parent" id="wallet">
                             <div className="tile is-child box has-text-centered">
-                            <p className="subtitle">Toggle below to search for new friends or filter through your current friends</p>
-                            <button className="button is-light is-medium deposit" id="newFreind" onClick={handleToggle}>{pickSearch ? "Switch to Find new friends" : "Switch to Search my friends"}</button>
-                            {!pickSearch ? 
+                            {/* <p className="subtitle">Toggle below to search for new friends or filter through your current friends</p> */}
+                            {/* <button className="button is-light is-medium deposit" id="newFreind" 
+                             onClick={handleToggle}
+                            >
+                            {pickSearch ? "Switch to Find new friends" : "Switch to Search my friends"}</button> */}
+                            {/* {!pickSearch ?  */}
+                            <p className="title">Search for new friends</p>
                                 <FindNewFriends/> 
-                                :
-                                <SearchMyFriends/> 
-                            }
+                                {/* // : */}
+                                {/* <SearchMyFriends/>  */}
+                            {/* // } */}
                             </div>
                             </div>
                         </div>
