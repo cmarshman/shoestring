b9856babdd513bcdf0581946b44c872f7de5fd52
JT Turner <j.tturner14@yahoo.com>
2020-04-15 15:53:47 -0400

    updated payments

--- client/src/components/TransferMoneyCard/index.js
+++ client/src/components/TransferMoneyCard/index.js
@@ -4,169 +4,174 @@ import MyImage from '../MyImage';
 import UserNameCard from '../UserNameCard';
 import httpClient from '../../httpClient';
 import * as Yup from 'yup';
-import { useFormik} from 'formik';
-import $ from 'jquery'
+import { useFormik } from 'formik';
+import $ from 'jquery';
+import moment from 'moment';
 
 
-   //Setup  validation condition on the schema using Yup
-   const validationSchenma = Yup.object({
-    name:   Yup.string().required(),
+//Setup  validation condition on the schema using Yup
+const validationSchenma = Yup.object({
+    name: Yup.string().required(),
     amount: Yup.number().required(),
     message: Yup.string().required(),
-     
+
 });
-     
-function TransferMoneyCard () {
+
+function TransferMoneyCard() {
 
     const { values, touched, errors, handleChange, handleBlur, handleSubmit } = useFormik({
         initialValues: {
             email: '',
             amount: null,
             message: '',
-             
+
         },
         validationSchenma,
         onSubmit(values) {
-        console.log('values', values)
-        transferMoney(values)        
+            console.log('values', values)
+            transferMoney(values)
         }
     });
- 
+
 
     const [currentUserObj, setCurrentUserObj] = useState({
         currentUser: httpClient.getCurrentUser()
     })
 
-  console.log("current", currentUserObj)
+    var createdDate = currentUserObj.currentUser.date
+    console.log(createdDate)
+    createdDate = moment().format('LL');
+
+    console.log("current", currentUserObj)
     // Function to update the image in the database
-    const updateUser = () =>{
+    const updateUser = () => {
         httpClient.InsertUpdate({
-            _id:  currentUserObj.currentUser._id,
+            _id: currentUserObj.currentUser._id,
             amount: values.amount,
             message: values.message
         })
     }
-     
 
- 
 
-//Function to handle the transfert money form
-//const transferMoney = () => {
+
+
+    //Function to handle the transfert money form
+    //const transferMoney = () => {
 
     const usersFriends = currentUserObj.friends;
     console.log("friends", usersFriends)
     //Function 
-   // let currentAmount = currentUserObj.amount
-   // let newamount = currentAmount + values.amount
+    // let currentAmount = currentUserObj.amount
+    // let newamount = currentAmount + values.amount
 
-    
-    const transferMoney = (evt) =>{
-        const userEmail = values.email 
-        httpClient.FindAllUser()   
-        .then(serverResponse => {
-          const data = serverResponse.data
-          let findEmail=data.find(item =>item.email===userEmail)
-            console.log("find", findEmail)
-            if(findEmail === undefined){
-                console.log("findEmail", findEmail)
-                $('#errormsg').attr("style", "color:red")
-                $('#errormsg').text("Email not found- try again.");
-                return
-            }
-            //Insert the new password after update
-            httpClient.InsertUpdate({
-                _id:  findEmail._id,
-                amount: values.amount,
-                message: values.message
+
+    const transferMoney = (evt) => {
+        const userEmail = values.email
+        httpClient.FindAllUser()
+            .then(serverResponse => {
+                const data = serverResponse.data
+                let findEmail = data.find(item => item.email === userEmail)
+                console.log("find", findEmail)
+                if (findEmail === undefined) {
+                    console.log("findEmail", findEmail)
+                    $('#errormsg').attr("style", "color:red")
+                    $('#errormsg').text("Email not found- try again.");
+                    return
+                }
+                //Insert the new password after update
+                httpClient.InsertUpdate({
+                    _id: findEmail._id,
+                    amount: values.amount,
+                    message: values.message
+                })
+                    // const updateAmount = () =>{
+                    //     let newamount = amount + values.amount
+                    // }
+                    // .then(httpClient.InsertUpdate({
+                    //     _id:  currentUserObj.currentUser._id,
+                    //     friends: [...currentUserObj.currentUser.friends, {amount: values.amount, message: values.message  }]
+                    // })
+                    // )
+                    .then(response => {
+                        console.log('response', response)
+                    })
+                    .then(window.location.replace('/home'))
+                    .catch(err => console.log('err', err))
             })
-            // const updateAmount = () =>{
-            //     let newamount = amount + values.amount
-            // }
-            // .then(httpClient.InsertUpdate({
-            //     _id:  currentUserObj.currentUser._id,
-            //     friends: [...currentUserObj.currentUser.friends, {amount: values.amount, message: values.message  }]
-            // })
-            // )
-            .then(response => {
-                console.log('response', response)
-            })
-            .then(window.location.replace('/home'))
-            .catch(err => console.log('err', err))
-        })
     }
 
-       //console.log("value", values)  
+    //console.log("value", values)  
 
-    return(
-        
+    return (
+
         <form onSubmit={handleSubmit}>
-        <div className="outerTile">
-            <div className="is-clearfix columns is-centered">
-                <div className="tile is-9 container column is-fluid">
-                    <div className="tile is-vertical is-parent" >
-                        <div className="tile is-child box has-text-centered" >
-                            <a className="title is-centered" href='/home'>
-                                <MyImage />
-                            </a>
-                            <br />
-                            <UserNameCard />
-                            <p id="funds">Funds Available: $100</p>
-                            <p id="member">Member Since: April 2019</p>
+            <div className="outerTile">
+                <div className="is-clearfix columns is-centered">
+                    <div className="tile is-9 container column is-fluid">
+                        <div className="tile is-vertical is-parent" >
+                            <div className="tile is-child box has-text-centered" >
+                                <a className="title is-centered" href='/home'>
+                                    <MyImage />
+                                </a>
+                                <br />
+                                <UserNameCard />
+                                <p id="funds">Funds Available: {currentUserObj.currentUser.amount}</p>
+                                <p id="member">Member Since: {createdDate}</p>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div className="tile is-ancestor columns is-centered" id="waldos_nothere">
-                <div className="tile is-vertical column is-two-fifths banana box" id="tile1">
-                    <p className="subtitle has-text-centered">
-                        Transfer money to your friend
+                <div className="tile is-ancestor columns is-centered" id="waldos_nothere">
+                    <div className="tile is-vertical column is-two-fifths banana box" id="tile1">
+                        <p className="subtitle has-text-centered">
+                            Transfer money to your friend
                     </p>
-                    <p>Enter your friend's email to start the transfer</p>
-                    <p className="control has-icons-left">
-                        <input className="input" type="text"  placeholder="Enter your  friend's email . . . " 
-                         onChange={handleChange}
-                         name="email"
-                         value={values.email}
-                         onBlur={handleBlur} />
-                        <div id='errormsg'></div>
-                        <span className="icon is-small is-left">
-                            <i className="fas fa-user-circle"></i>
-                        </span>
-                    </p>
-                    <p>Enter the amount you would like to transfer</p>
-                    <p className="control has-icons-left">
-                        <input className="input" type="text" placeholder="$50" 
-                        onChange={handleChange}
-                        name="amount"
-                        value={values.amount}
-                        onBlur={handleBlur} />
-                        {/* {values.amount.length <1 &&  touched.amount && 'errors' ? (
+                        <p>Enter your friend's email to start the transfer</p>
+                        <p className="control has-icons-left">
+                            <input className="input" type="text" placeholder="Enter your  friend's email . . . "
+                                onChange={handleChange}
+                                name="email"
+                                value={values.email}
+                                onBlur={handleBlur} />
+                            <div id='errormsg'></div>
+                            <span className="icon is-small is-left">
+                                <i className="fas fa-user-circle"></i>
+                            </span>
+                        </p>
+                        <p>Enter the amount you would like to transfer</p>
+                        <p className="control has-icons-left">
+                            <input className="input" type="text" placeholder="$50"
+                                onChange={handleChange}
+                                name="amount"
+                                value={values.amount}
+                                onBlur={handleBlur} />
+                            {/* {values.amount.length <1 &&  touched.amount && 'errors' ? (
                               <p className="errormsg">Please enter a valid amount</p>
                         ): ''} */}
-                        <span className="icon is-small is-left">
-                            <i className="fas fa-money-bill-wave-alt"></i>
-                        </span>
-                    </p>
-                    <p>Leave a messeage for your friend</p>
-                    <textarea className="textarea" placeholder="For fluffy rainbow unicorn"
-                    onChange={handleChange}
-                    name="message"
-                    value={values.message}
-                    onBlur={handleBlur} />
-                    {/* {values.message.length <10 &&  touched.amount && 'errors' ? ( */}
-                          {/* <p className="errormsg">Please enter a valid message</p>
+                            <span className="icon is-small is-left">
+                                <i className="fas fa-money-bill-wave-alt"></i>
+                            </span>
+                        </p>
+                        <p>Leave a messeage for your friend</p>
+                        <textarea className="textarea" placeholder="For fluffy rainbow unicorn"
+                            onChange={handleChange}
+                            name="message"
+                            value={values.message}
+                            onBlur={handleBlur} />
+                        {/* {values.message.length <10 &&  touched.amount && 'errors' ? ( */}
+                        {/* <p className="errormsg">Please enter a valid message</p>
                     ): ''} */}
-                     
-                    {/* </textarea> */}
-                    <br/>
-                    <a className="button is-light" id="deposit" type="submit"
-                    onClick={transferMoney}>Transfer Money</a>
 
+                        {/* </textarea> */}
+                        <br />
+                        <a className="button is-light" id="deposit" type="submit"
+                            onClick={transferMoney}>Transfer Money</a>
+
+                    </div>
                 </div>
             </div>
-        </div>
         </form>
-         
+
     )
 }
 
--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -7,55 +7,58 @@ import NavBarAuth from '../components/NavBarAuth';
 import UserNameCard from '../components/UserNameCard';
 import AddImage from './../components/AddImage';
 import Payments from './../components/Payments';
-// import Modal from '../components/Modal';
- 
+import moment from 'moment';
+
 
 function Landing() {
- 
-    const [currentUserObj, setCurrentUserObj] = useState({
-         currentUser: httpClient.getCurrentUser()
-    })
- 
 
- // Render the  all  the  pages on the landing pages
+    const [currentUserObj, setCurrentUserObj] = useState({
+        currentUser: httpClient.getCurrentUser()
+    })
+
+    var createdDate = currentUserObj.currentUser.date
+    console.log(createdDate)
+    createdDate = moment().format('LL');
+
+    // Render the  all  the  pages on the landing pages
     return (
         <>
-        {(currentUserObj.currentUser !==null) ?(
-            <div>
-              <NavBarAuth/> 
-                  <div className="outerTile">
-                      <div className="is-clearfix columns is-centered">
-                      <div className="tile is-9 container column is-fluid" id="purpleDuck">
-                          <div className="tile is-vertical is-parent" >
-                              <div className="tile is-child box has-text-centered" >
-                                  <div className="is-centered" >
-                                  <UserNameCard/>
-                                   <AddImage />
-                                  <br />
-                                  <Plaid />
-                                  <p id="funds">Funds Available: $100</p>
-                                  <p id="member">Member Since: April 2019</p>
-                                  </div>
-                              </div>
-                              <div className="tile is-child box is-fullwidth">
-                                  {/* <p className="title" id="paymentList">Payments</p>
+            {(currentUserObj.currentUser !== null) ? (
+                <div>
+                    <NavBarAuth />
+                    <div className="outerTile">
+                        <div className="is-clearfix columns is-centered">
+                            <div className="tile is-9 container column is-fluid" id="purpleDuck">
+                                <div className="tile is-vertical is-parent" >
+                                    <div className="tile is-child box has-text-centered" >
+                                        <div className="is-centered" >
+                                            <UserNameCard />
+                                            <AddImage />
+                                            <br />
+                                            <Plaid />
+                                            <p id="funds">Funds Available: {currentUserObj.currentUser.amount}</p>
+                                            <p id="member">Member Since: {createdDate}</p>
+                                        </div>
+                                    </div>
+                                    <div className="tile is-child box is-fullwidth">
+                                        {/* <p className="title" id="paymentList">Payments</p>
                                   <p>Lisa paid Arnold for Pizza</p>
                                   <p>Jeff paid Jesus for rent</p> */}
-                                  <Payments/>
-                              </div>
+                                        <Payments />
+                                    </div>
+                                </div>
                             </div>
-                      </div>
-                      <FriendCard />
-                  </div>
-              </div>
-         </div>
-             
-        ): window.location.replace("/")}
-        
-</>
-)
+                            <FriendCard />
+                        </div>
+                    </div>
+                </div>
+
+            ) : window.location.replace("/")}
+
+        </>
+    )
 
 }
 export default Landing;
- 
+
 
