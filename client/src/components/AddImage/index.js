805e25b2ff9b07c9c4546681314da419ab8abb10
Mamessile Kiman <sile.kiman@yahoo.com>
2020-04-23 13:23:53 -0400

    cleaned up

--- client/src/components/AddImage/index.js
+++ client/src/components/AddImage/index.js
@@ -11,23 +11,15 @@ function AddImage() {
         currentUser: httpClient.getCurrentUser()
     })
     //Set uploaded image state
-   let  myImage =currentUserObj.currentUser.image
-    
     const [image, setImage] = useState({
-             image: myImage
     });
     const [loading, setLoading] = useState(false);
     const [friendResult, setFriendResult] = useState([{}]);
 
-    //setFriendResult([{...friendResult, image:image}])
-
+ //Load funtion on page load
     useEffect(() => {
-        //findImage()
-        //handleInputChange())
         handleSearch()
-        //setFriendResult([{friendResult}])
-        //findImage()
-        //setImage([image])
+        
     }, [])
 
     //Function to setup image upload
@@ -53,87 +45,30 @@ function AddImage() {
             _id: currentUserObj.currentUser._id,
             image: file.secure_url
         })
-        //const handleInputChange = evt => {
-        const currentUserId = currentUserObj.currentUser._id
-        console.log("id:", currentUserId)
-        //let friendToAdd = friendResult.find(item => item._id === friendId)
-        //const value = event.target.value;
-        // httpClient.FindAllUser()
-        //     .then(response => {
-        //         const data = response.data
-        //         //setFriendResult(data);
-        //         //if (value !== "") {
-        //         let findImage = data.find(item => item._id === currentUserId)
-        //         setImage({image:findImage.image});
-        //         console.log('filteredArr', findImage)
-        //         //}
-        //     })
-        //     .catch(err => { console.log(err) })
-        //}
-
+         window.location.reload()
     }
-    //Variable to setup user
-    const findImage = () => {
-        httpClient.FindUser({
-            _id: currentUserObj.currentUser._id,
-            //image: image.image
-
-        }).then(data => {
-            setImage(data)
-            console.log("data", data)
-        })
-    }
-
+   
       //Function to load all user on page load
       const handleSearch = () => {
         httpClient.FindAllUser()
        
             .then(serverResponse => {
                 setFriendResult(serverResponse.data);
-                //let findImg = data.find(item => item._id === currentUserId)
                 let currentUserId = currentUserObj.currentUser._id
                 let findImg = serverResponse.data.find(item => item._id === currentUserId)
              setFriendResult([findImg])
+              
             })
             .catch(err => { console.log(err) })            
     }
 
-
-    const handleInputChange = () => {
-        const currentUserId = currentUserObj.currentUser._id
-        console.log("id:", currentUserId)
-        //let friendToAdd = friendResult.find(item => item._id === friendId)
-        //const value = event.target.value;
-        httpClient.FindAllUser()
-            .then(response => {
-                const data = response.data
-                setFriendResult(data);
-                //if (value !== "") {
-                let findImage = data.find(item => item._id === currentUserId)
-                setFriendResult([{findImage}]);
-                console.log('filteredArr', findImage)
-                //}
-            })
-            .catch(err => { console.log(err) })
-    }
-
-    console.log('friendResult', image)
-    //variable to setup the image to display
-    const currentUserID = currentUserObj.currentUser._id
-    const my_image = image.image
-    console.log('db image', my_image)
-
-    //let findOne = friendResult.find(item => item._id === currentUserID)
-    //let findOne ;
-    console.log('db image', friendResult)
     //Function to render page with uploaded image
     return (
 
         <>
-        {/* { findOne = friendResult.find(item => item._id === currentUserID)} */}
-            {currentUserObj.currentUser !== null ? (
+             {currentUserObj.currentUser !== null ? (
                 <div>
-                    {/* <br /> */}
+                    <br />
 
                     { friendResult.map(item =>{ 
                          return (
@@ -151,7 +86,7 @@ function AddImage() {
                                     name="file"
                                     placeholder="Upload image"
                                     onChange={uploadImage}
-                                    //onInput={handleInputChange}
+                                     
                                 />
                             </div>
                           )
@@ -159,7 +94,6 @@ function AddImage() {
                           )}  
                 </div>
                ): window.location.replace("/")}
-            {/* ) */}
         </>
     )
 }
