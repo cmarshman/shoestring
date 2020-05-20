46decdde800df69544abef9762468139b15b30a9
Mamessile Kiman <sile.kiman@yahoo.com>
2020-05-19 21:09:04 -0400

    added changes to handle the phone number field to only take number

--- client/src/components/FindNewFriends/index.js
+++ client/src/components/FindNewFriends/index.js
@@ -54,7 +54,7 @@ function FindNewFriends(currentUser) {
     function onSuccess() {
         $("#successMsg").text("Your new friend has been added!")
     }
-
+   
     //update the database with a new friend added
     const addfriend = (evt) => {
         const friendId = evt.target.dataset.newfriend
@@ -62,11 +62,51 @@ function FindNewFriends(currentUser) {
         // newFriendAdded(addNewFriend => !addNewFriend);
         httpClient.InsertUpdate({
             _id: currentUserObj.currentUser._id,
-            friends: [...currentUserObj.currentUser.friends, { _id: friendToAdd._id, image: friendToAdd.image, name: friendToAdd.name, city: friendToAdd.city, state: friendToAdd.state, amount: friendToAdd.amount, message: friendToAdd.message,  }]
+           friends: [...currentUserObj.currentUser.friends, {...friendToAdd}]
+            //friends: currentUserObj.currentUser.friends.push(friendToAdd.friends)
+
+            //friends: [...currentUserObj.currentUser.friends, { _id: friendToAdd._id, image: friendToAdd.image, name: friendToAdd.name, city: friendToAdd.city, state: friendToAdd.state, amount: friendToAdd.amount, message: friendToAdd.message, date: friendToAdd.date}]
         })
+        //setFriendResult(filteredArr);
         // alert("Friend added!");
         onSuccess();
     }
+
+var btnEL = document.querySelectorAll(".saveBtn");
+
+const friendsButtonHandle = () =>{ 
+
+    console.log("buttons", btnEL )
+    // Add an EventListner to save the events into local storage on click
+        btnEL.forEach(function (event) {
+        event.addEventListener("click", addfriend)
+
+   })
+};
+
+//Function to load all user on page load
+const handleFriends = (evt) => {
+    const friendId = evt.target.dataset.newfriend
+    httpClient.FindAllUser()
+        .then(serverResponse => {
+           setFriendResult(serverResponse.data);
+            //let currentUserId = currentUserObj.currentUser._id
+            let findFriend = serverResponse.data.find(item => item._id === friendId)
+            let friendsArray = findFriend.friends
+           let container = currentUserObj.currentUser.friends.push(friendsArray)
+           // setFriendResult(friendsArray)
+           for(var i =0; i<container.length; i++){
+             httpClient.InsertUpdate({
+            _id: currentUserObj.currentUser._id,
+            friends: [...currentUserObj.currentUser.friends, {...findFriend}]
+         
+            })
+        }
+            //setFriendResult( [...currentUserObj.currentUser.friends, {...findFriend}])
+        })
+        .catch(err => { console.log(err) })            
+}
+
 
     //Function to load all user on page load
     const handleSearch = () => {
@@ -94,7 +134,8 @@ function FindNewFriends(currentUser) {
                                     <p className="subtitle" >{item.name}</p>
                                     <p className="" >{item.city}, {item.state}</p>
                                     <hr />
-                                    <button className="button is-fullwidth is-dark is-medium" id="friend" data-newfriend={item._id} onClick={addfriend}>{addNewFriend ? "Friend Added!" : "Add Friend"}
+                                    <button className="button is-fullwidth is-dark is-medium saveBtn" id="friend" data-newfriend={item._id} onClick ={friendsButtonHandle}>
+                                        Add Friend
                                     </button>
                                 </article>
                             </div>
