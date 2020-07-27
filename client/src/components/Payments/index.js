335c0ea160bd92ef476c039de38a021951c2ec25
Mamessile Kiman <sile.kiman@yahoo.com>
2020-07-26 21:47:40 -0400

    add functions to show all current user transactions

--- client/src/components/Payments/index.js
+++ client/src/components/Payments/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import httpClient from "../../httpClient";
 import './style.css';
 
-function Payments(currentUser) {
+function Payments() {
     const [currentUserObj, setCurrentUserObj] = useState({
         currentUser: httpClient.getCurrentUser()
     });
@@ -13,51 +13,24 @@ function Payments(currentUser) {
 
     }, [])
 
-    currentUser = [
-        {
-            _id: currentUserObj.currentUser._id,
-            friends: currentUserObj.currentUser.friends,
-            name: currentUserObj.currentUser.name,
-            phone: currentUserObj.currentUser.phone,
-            city: currentUserObj.currentUser.city,
-            state: currentUserObj.currentUser.state,
-            email: currentUserObj.currentUser.email,
-            password: currentUserObj.currentUser.password,
-            image: currentUserObj.currentUser.image,
-            amount: currentUserObj.currentUser.amount,
-            message: currentUserObj.currentUser.message,
-             
-        }
-    ]
-
-
-    const usersFriends = currentUserObj.currentUser.friends;
-
-    const friendsPayments = currentUserObj.currentUser.friends.slice(1);
-
-    // console.log(friendsPayments);
-    const userName = currentUserObj.currentUser.name;
-
-    const handleSearch = () => {
+    
+    //set data on load
+     const handleSearch = () => {
         httpClient.FindAllUser()
 
             .then(serverResponse => {
-                setFriendResult(serverResponse.data);
                 let currentUserId = currentUserObj.currentUser._id
                 let findPayment = serverResponse.data.find(item => item._id === currentUserId)
-                let findFriend = findPayment.friends.slice(1)
-                console.log(findPayment)
-                setFriendResult([findPayment])
-
+                let findFriend = findPayment.sentTransactions.slice(1)
+                console.log(findFriend)
+                setFriendResult(findFriend)
+                setCurrentUserObj(findPayment)
 
             })
             .catch(err => { console.log(err) })
     }
 
-    // console.log("hello", friendResult)
-    // console.log(currentUserObj.currentUser.message)
-    // console.log(friendResult)
-
+   //map all the tranctions for current user transactions
     return (
         <div className="tile is-child box is-fullwidth" id="payments">
             <div>
@@ -68,12 +41,10 @@ function Payments(currentUser) {
                             <article key={item._id} className="media is-scrollable" id="paymentList">
                                 <figure className="media-left">
                                     <p className="image is-square is-48x48">
-                                        <img className="is-rounded" src={item.image} alt={item.name} />
+                                        <img className="is-rounded" src={currentUserObj.image} alt={currentUserObj.name} />
                                     </p>
-                                    {/* <img className="is-rounded is-48x48" id="friendImage" src={item.image} alt={item.name} />
-                                    <p>{item.name} paid {userName} for {item.message}</p> */}
                                 </figure>
-                                <div className="transactionInfo">{item.name} just got paid for {item.message}</div>
+                                <div className="transactionInfo">{currentUserObj.name} just paid {item.name} for {item.message}</div>
 
                                 <hr />
                             </article>
